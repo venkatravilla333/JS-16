82847704f3aef293a40909151ee91a1a6a8c84af
venkat ravilla <venkatravilla333@gmail.com>
2025-04-26 19:34:56 +0530

    operators

---
+++ 1.Basics/index.js
@@ -0,0 +1,109 @@
+//primitives
+
+// var a =
+//   'HP 15s, 12th Gen Intel Core i3-1215U Laptop (8GB DDR4, 512GB SSD) Anti-Glare, 15.6"/39.6cm,FHD, Win 11, MS Office 21, Silver, 1.69kg, Intel UHD Graphics, HD Camera, Dual Speakers, fy5006tu';
+
+// console.log(a);
+
+// var b = 20000;
+// console.log(b);
+
+// var c = true;
+// console.log(c);
+
+// var d;
+
+// console.log(d);
+
+// var e = null;
+
+// console.log(e);
+
+//reference
+
+// var name = 'sachin'
+// var age = 40
+// var isPlayed = true
+// var height = 5
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   isPlayed: true,
+//   play: function () {
+//     console.log('play');
+//   },
+//   height: 5,
+// };
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person['age']);
+// console.log(person.isPlayed);
+// console.log(person.height);
+// person.play();
+
+//array
+
+// var arr = [1, 2, 3, 'sachin', true];
+
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+
+// function
+
+// function test() {
+//   var x = 1
+// }
+
+// test()
+
+
+//exp
+
+// var x = function hello() {
+//   var r = 10
+// }
+// x()
+
+// var y = function () {
+//   var x = 100
+// }
+// y()
+
+// var z =  () => {
+//   var x = 1000
+// }
+// z()
+
+
+//Type of operator
+
+
+// var a = 'sachin'
+// console.log('value:',a, 'datatype:', typeof a)
+// var b = 40
+// console.log('value:',b, 'datatype:', typeof b)
+// var c = true
+// console.log('value:',c, 'datatype:', typeof c)
+// var d
+// console.log('value:',d, 'datatype:', typeof d)
+// var e = null
+// console.log('value:', e, 'datatype:', typeof e)
+
+// var f = { value: 100 }
+// console.log('value:', f, 'datatype:', typeof f)
+
+// var g = [1, 2, 3]
+// console.log('value:', g, 'datatype:', typeof g)
+
+// var h = () => {
+  
+// }
+// console.log('value:', h, 'datatype:', typeof h)
---
+++ 2.Operators/index.js
@@ -0,0 +1,133 @@
+
+
+//arithmatic operators
+// var x = 20
+
+// var y = 10
+
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+// console.log(x ** y)
+
+// console.log(x)
+// //incremental
+// console.log(++x)
+// console.log(++x)
+// console.log(++x)
+// console.log(x++)
+// console.log(x)
+// console.log(++x)
+// console.log(x--)
+// console.log(x)
+
+//assignment operator
+
+// var x = 20
+// var y = 10
+
+// console.log(x)
+// console.log(++x)
+// console.log(++x)
+// console.log(++x)
+// console.log(++x)
+// console.log(++x)
+
+// console.log(x += 5)
+// console.log(x -= 5)
+// console.log(x *= 5)
+// console.log(x /= 5)
+// console.log(x %= 5)
+// console.log(x **= 5)
+
+//comparision operators
+
+// var x = 10
+
+// var y = 20
+
+//relational
+
+// console.log(x > y)
+// console.log(x >= y)
+// console.log(x < y)
+// console.log(x <= y)
+
+//equality
+
+// var x = 10
+// var y = 11
+// var y = 10
+// var y = '10'
+
+// var x = 10
+// var y = x
+// console.log(x == y)
+// console.log(x === y)
+
+// var x = undefined
+// var y = null
+
+// var x = {}
+// var y = {}
+// var x = {value: 10}
+// var y = {value: 10}
+// var x = {value: 10}
+// var y = {value: 20}
+// var x = {value: 10}
+// var y = x
+
+// var x = 10
+
+// var y = {value: 10}
+// var x = {value: 10}
+// var y = x
+
+// var x = [1,2,3]
+// var y = [1,2,3]
+
+// var x = [1, 2, 3]
+
+// var y = [1, 2, 3, 4]
+
+// var x = ()=>{}
+// var y = ()=>{}
+
+// var x = ()=>{}
+// var y = x
+
+// console.log(x == y) //loose (only values)
+// console.log(x === y) //strict (values & data types)
+
+
+//inequality
+
+// var x = 10
+// var y = '10'
+
+// console.log(x == y) //true
+// console.log(x != y) //false
+// console.log(x === y)
+// console.log(x !== y)
+
+//ternary oprator
+
+// var age = 19
+
+// var res = age >= 18 ? 'having vote' : 'not having vote'
+// console.log(res)
+
+// Logical operators 
+
+
+
+
+
+
+
+
+
+
+
