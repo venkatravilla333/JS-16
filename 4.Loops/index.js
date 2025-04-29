de07815549a9259ca0275aa697fca02021afbaba
venkat ravilla <venkatravilla333@gmail.com>
2025-04-29 19:36:14 +0530

    loops

---
+++ 4.Loops/index.js
@@ -0,0 +1,147 @@
+
+
+var x = 'sachin'
+
+
+//without using loops
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+// console.log(x)
+
+//for loop
+
+// for (var i = 1; i <= 100; i++){
+//   if(i === 50) continue
+//   console.log(i, x)
+// }
+
+//while loop
+
+// var i = 1
+
+// while (i<=10) {
+//   console.log(i, x)
+//   i++
+// }
+
+
+//do while loop
+
+// var i = 11
+
+// do {
+//   console.log(i, x)
+//   i++
+// } while (i <= 10)
+
+
+// var a = { value: 10 }
+
+// console.log(a)
+
+// var b = [1, 2, 3]
+
+// console.log(b)
+
+
+
+
+
