50a8761d9df8e12fae7ac31ceb44bb9e9a12c8c1
venkat ravilla <venkatravilla333@gmail.com>
2025-05-02 19:54:21 +0530

    scope & hoisting

--- 5.Functions/index.js
+++ 5.Functions/index.js
@@ -77,7 +77,7 @@
 // test(1, 2, 3, 4, 5)
 
 
-function test (a, ...rest) {
+// function test (a, ...rest) {
   // console.log(rest)
   // console.log(rest[0])
   // console.log(rest[1])
@@ -85,14 +85,148 @@ function test (a, ...rest) {
   // console.log(rest[3])
   // console.log(rest[4])
 
-  var sum = 0
-  for (var value of rest) {
-    console.log(value)
-    sum += value
-  }
-  console.log(sum)
-}
+//   var sum = 0
+//   for (var value of rest) {
+//     console.log(value)
+//     sum += value
+//   }
+//   console.log(sum)
+// }
 
-test(1, 2, 3, 4, 5)
+// test(1, 2, 3, 4, 5)
+
+
+//scope
+
+//storage (Global)
+
+// var a = 1
+// let b = 2
+// const c = 3
+
+//access (Global)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// function test() {
+//   //storage
+//   var d = 4
+//   let e = 5
+//   const f = 6
+//   //access
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+
+  
+
+
+//   if (true) {
+
+//     //storage
+//     var g = 7
+//     let h = 8
+//     const i = 9
+//     // access
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     console.log(d)
+//     console.log(e)
+//     console.log(f)
+//     console.log(g)
+//     console.log(h)
+//     console.log(i)
+//   }
+//     console.log(g)
+//     // console.log(h)
+//     // console.log(i)
+
+// }
+// test()
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+
+
+console.log(a)
+// let a = 'sachin'
+console.log(a)
+console.log(b)
+var b = 40
+console.log(b)
+
+console.log(c)
+var c = true
+console.log(c)
+
+console.log(d)
+var d 
+console.log(d)
+
+console.log(e)
+var e = null
+console.log(e)
+
+console.log(f)
+var f = { name: 'kohli' }
+console.log(f)
+
+console.log(g)
+var g = [1, 2, 3]
+console.log(g)
+
+console.log(h)
+var h = function test() {
+  console.log('named')
+}
+// h()
+console.log(h)
+
+console.log(i)
+// i()
+var i = function () {
+  console.log('ananymous')
+}
+// i()
+console.log(i)
+
+console.log(j)
+var j = () => {
+  console.log('arrow')
+}
+// j()
+console.log(j)
+
+console.log(declare)
+function declare() {
+  console.log('Declare')
+}
+declare()
+console.log(declare)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
